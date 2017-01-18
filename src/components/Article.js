9bec930eea45c894874cf687761ec7f447ea5f85
Jakub Kłobus <souhepend@hotmail.com>
2017-01-18 14:07:18 +0100

    Article better styling

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,7 +1,7 @@
 /* @flow */
 
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, WebView } from 'react-native';
 
 import type { Article } from '../types';
 
@@ -9,13 +9,56 @@ type TProps = {
   article: Article;
 }
 
-export default function(props: TProps) {
+export default function (props: TProps) {
   const { article } = props;
   return (
-    <View>
-      <Text>{article.title}</Text>
-      {article.creator && <Text>Creator: {article.creator}</Text>}
-      <Text>{article.pubDate.toGMTString()}</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>{article.title}</Text>
+      <View style={styles.info}>
+        {article.creator && <Text style={styles.infoItem}>Creator: {article.creator}</Text>}
+        <Text style={styles.infoItem}>Date: {article.pubDate.toGMTString()}</Text>
+      </View>
+      <View style={styles.content}>
+        {(article.description || article.encoded) && (
+          <View style={styles.webviewContainer}>
+            <WebView
+              style={styles.webview}
+              startInLoadingState
+              source={{ html: article.description || article.encoded, baseUrl: 'http://localhost:8080' }}
+            />
+          </View>
+        )}
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    margin: 10,
+    paddingLeft: 10,
+    borderLeftWidth: 4,
+    borderLeftColor: '#004b8b',
+  },
+  title: {
+    fontSize: 18,
+    color: '#004b8b',
+  },
+  info: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  infoItem: {
+    fontSize: 13,
+    color: 'gray',
+  },
+  webviewContainer: {
+    marginTop: 10,
+    height: 150,
+    borderWidth: 1,
+    borderColor: '#004b8b',
+  },
+  webview: {
+    flex: 1,
+  },
+});
